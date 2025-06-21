df0a44ef116330f294b7b58b1bbc4ef7138112b4
Bhanu Prasad Nampally <jayajayaanth@gmail.com>
2025-06-21 12:03:28 +0530

    Fix: rewrite queries.js to work with better-sqlite3

--- models/queries.js
+++ models/queries.js
@@ -1,45 +1,45 @@
-const db = require("./database");
+const db = require('./database');
 
 module.exports = {
   // Get all restakers
-  getAllRestakers: (callback) => {
-    db.all("SELECT * FROM restakers", [], callback);
+  getAllRestakers: () => {
+    const stmt = db.prepare("SELECT * FROM restakers");
+    return stmt.all(); // returns an array of rows
   },
 
   // Add a restaker
-  addRestaker: ({ address, amount, validator }, callback) => {
-    db.run(
-      "INSERT INTO restakers (address, amount, validator) VALUES (?, ?, ?)",
-      [address, amount, validator],
-      callback
+  addRestaker: ({ address, amount, validator }) => {
+    const stmt = db.prepare(
+      "INSERT INTO restakers (address, amount, validator) VALUES (?, ?, ?)"
     );
+    return stmt.run(address, amount, validator); // returns info about insertion
   },
 
   // Get all validators
-  getAllValidators: (callback) => {
-    db.all("SELECT * FROM validators", [], callback);
+  getAllValidators: () => {
+    const stmt = db.prepare("SELECT * FROM validators");
+    return stmt.all();
   },
 
   // Add a validator
-  addValidator: ({ operator_id, address, total_stake, status, slash_history }, callback) => {
-    db.run(
-      "INSERT INTO validators (operator_id, address, total_stake, status, slash_history) VALUES (?, ?, ?, ?, ?)",
-      [operator_id, address, total_stake, status, JSON.stringify(slash_history)],
-      callback
+  addValidator: ({ operator_id, address, total_stake, status, slash_history }) => {
+    const stmt = db.prepare(
+      "INSERT INTO validators (operator_id, address, total_stake, status, slash_history) VALUES (?, ?, ?, ?, ?)"
     );
+    return stmt.run(operator_id, address, total_stake, status, JSON.stringify(slash_history));
   },
 
   // Get reward for a specific user
-  getRewardByUser: (user, callback) => {
-    db.get("SELECT * FROM rewards WHERE user = ?", [user], callback);
+  getRewardByUser: (user) => {
+    const stmt = db.prepare("SELECT * FROM rewards WHERE user = ?");
+    return stmt.get(user); // returns one object or undefined
   },
 
   // Add reward info
-  addReward: ({ user, total_rewards, breakdown }, callback) => {
-    db.run(
-      "INSERT INTO rewards (user, total_rewards, breakdown) VALUES (?, ?, ?)",
-      [user, total_rewards, JSON.stringify(breakdown)],
-      callback
+  addReward: ({ user, total_rewards, breakdown }) => {
+    const stmt = db.prepare(
+      "INSERT INTO rewards (user, total_rewards, breakdown) VALUES (?, ?, ?)"
     );
-  }
+    return stmt.run(user, total_rewards, JSON.stringify(breakdown));
+  },
 };
